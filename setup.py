2a60d33f64eff5085c548612a0aa6e86f00a5520
NtemKenyor <nkenyor@gmail.com>
2024-02-25 20:24:14 +0100

    making the setup contain some description.

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='tea_AI',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
+    author_email="nkenyor@gmail.com",
+    description="This is a simple package for the Tea Airdrops and development. This is an AI generative library and we have great plans to build some awesome idea here for AI and Blockchain Engineers.",
+    url = "https://github.com/NtemKenyor/tea_AI",
     entry_points={
         'console_scripts': [
             'make_tea=tea_AI.tea:make_tea'
