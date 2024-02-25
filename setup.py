63de5045c8234c617f080a960e281b1ade339ada
NtemKenyor <nkenyor@gmail.com>
2024-02-25 20:37:47 +0100

    fixing some versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tea_AI',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     author_email="nkenyor@gmail.com",
     description="This is a simple package for the Tea Airdrops and development. This is an AI generative library and we have great plans to build some awesome idea here for AI and Blockchain Engineers.",
