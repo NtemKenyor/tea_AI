90713df4c88ba6a85c45591cb3fd6e1665459944
NtemKenyor <nkenyor@gmail.com>
2024-02-26 20:03:46 +0100

    changest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.2',
     packages=find_packages(),
     author_email="nkenyor@gmail.com",
-    description="This is a simple package for the Tea Airdrops and development. This is an AI generative library and we have great plans to build some awesome idea here for AI and Blockchain Engineers.",
+    description="This is a simple package for the Tea Airdrops and development.",
     url = "https://github.com/NtemKenyor/tea_AI",
     entry_points={
         'console_scripts': [
