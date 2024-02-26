97ad07f82f35d342c40e04e1380cad380fbb7d75
NtemKenyor <nkenyor@gmail.com>
2024-02-26 20:32:34 +0100

    still making the dance. let's go..

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='tea_AI',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     author_email="nkenyor@gmail.com",
     description="This is a simple package for the Tea Airdrops and development.",
     url = "https://github.com/NtemKenyor/tea_AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={
         'console_scripts': [
             'make_tea=tea_AI.tea:make_tea'
